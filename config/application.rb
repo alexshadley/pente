50f644f24719e4ce04bd2c563624f360c6f5586a
Alex Shadley <alex@rideos.ai>
2021-03-16 17:47:37 -0500

    adds more deps back in

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,9 @@
 require_relative "boot"
 
 require 'action_cable/engine'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'active_model/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
